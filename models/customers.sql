765f95da3ed53ec9a894739fc63e1d4a3f801c05
sathyaroyal1233@gmail.com <sathyaroyal1233@gmail.com>
2026-01-15 14:11:45 +0000

    cmt

--- models/customers.sql
+++
@@ -1,5 +0,0 @@
-select
-  'C1' as customer_id,
-  'T1' as tenant_id,
-  current_timestamp as created_at,
-  'ACTIVE' as customer_status
--- models/orders.sql
+++
@@ -1,6 +0,0 @@
-select
-  1 as order_id,
-  'T1' as tenant_id,
-  100 as order_amount,
-  current_timestamp as created_at,
-  'COMPLETED' as status
