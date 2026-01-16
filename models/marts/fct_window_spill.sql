5ac2ed5dedf3c0377660afbf1e1d60380b8495a5
sathyaroyal1233@gmail.com <sathyaroyal1233@gmail.com>
2026-01-16 08:43:44 +0000

    cccc

--- models/marts/fct_window_spill.sql
+++ models/marts/fct_window_spill.sql
@@ -1,12 +1,16 @@
 {{ config(materialized='table') }}
 
 -- dbt_model: fct_window_spill
--- expected_behavior: window_function_spill
+-- expected_behavior: severe_window_spill_on_xsmall
 
 SELECT
     l_partkey,
     l_orderkey,
+    l_comment,
     SUM(l_extendedprice)
-        OVER (PARTITION BY l_partkey ORDER BY l_orderkey)
-        AS running_revenue
-FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF10.LINEITEM
+        OVER (
+            PARTITION BY l_partkey
+            ORDER BY l_comment
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) AS running_revenue
+FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF10.LINEITEM;
